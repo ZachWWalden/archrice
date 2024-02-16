2f65aa1711e32181c0f5442b235ba95f1d6ac78d
Zachary Walden <zachary.walden@eagles.oc.edu>
2024-02-16 12:25:32 -0600

    Add keymap to inspect treesitter ast

---
+++ .config/nvim/lua/zachwalden/lazy/treesitter.lua
@@ -0,0 +1,24 @@
+return	{
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function ()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust", "c_sharp", "bash", "cpp", "java", "latex", "markdown", "make", "python", "verilog", "vim" },
+				sync_install = false,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				indent = { enable = true },
+			})
+			vim.keymap.set('n', '<leader>vast', "<cmd>InspectTree")
+		end
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+	},
+}
+
